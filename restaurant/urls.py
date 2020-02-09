e768c0af45f2b2b2d3cccaf71e249f7a02e79878
Milosz Bukala <mil.bukala@gmail.com>
2020-02-09 12:00:55 +0100

    Apka dot. rezerwacji

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings    # <--------
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('reservation/', include('reservation.urls')),
     # path('accounts/', include('django.contrib.auth.urls'))
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
