c6c7819a4432bf65429d408987d9fa1b1767600c
Milosz Bukala <mil.bukala@gmail.com>
2020-05-13 15:33:47 +0200

    Total change of the layout of the website. There was a problem with the settings, but I managed to fix it. Homepage is almost ready. All images are uploaded on my imgur server.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -1,166 +1,10 @@
-# import os
-
-# import environ
-
-# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# # directory to file mamnage.py
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))      #sciezka absolutna do pliku manage.py
-
-# env = environ.Env(
-#     #defaul types and values
-#     DEBUG=(bool, False),
-#     ALLOWED_HOSTS=(list, []),
-# )
-# # .env is in parent directory to 'settings.py'
-# env_file= os.path.join(BASE_DIR, '.env')     #sciezka absolutna do pliku env
-# # read from .env file if it exists
-# environ.Env.read_env(env_file)     #laduje zmienne z pliku env do naszego srodowiska
-
-# DEBUG = env('DEBUG')
-# SECRET_KEY = env('SECRET_KEY')
-# ALLOWED_HOSTS = env('ALLOWED_HOSTS')
-
-# # Application definition
-
-# INSTALLED_APPS = [
-#     'account.apps.AccountConfig',
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'jquery',
-#     'debug_toolbar',            # bardzo wazne, zeby to bylo za staticfiles!
-#     'django_extensions',
-#     'sass_processor',
-#     'compressor',
-#     'taggit',
-#     'bootstrap4',
-#     'tempus_dominus',
-#     'reservation.apps.ReservationConfig',
-#     'homepage.apps.HomepageConfig',
-#     'menu.apps.MenuConfig',
-#     'blog.apps.BlogConfig',
-
-# ]
-
-# MIDDLEWARE = [
-#     'debug_toolbar.middleware.DebugToolbarMiddleware',      # <---- to tez dopisujemy na samej gorze to doinsatalowaniu paczek
-#     'django.middleware.security.SecurityMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# ROOT_URLCONF = 'restaurant.urls'
-
-# INTERNAL_IPS = [
-#     '127.0.0.1', 
-# ]
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'restaurant.wsgi.application'
-
-# # Database
-# # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     # }
-#     # zamieniamy dla heroku tak jak ponizej:
-#     'default': env.db(),
-# }
-
-# # Password validation
-# # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-# TIME_ZONE = 'UTC'
-# USE_I18N = True
-# USE_L10N = True
-# USE_TZ = True
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATICFILES_FINDERS = [         
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#     'sass_processor.finders.CssFinder',
-#     'compressor.finders.CompressorFinder',
-    
-# ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# SASS_PROCESOR_ROOT = STATIC_ROOT
-# STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# # STATICFILES_DIRS = [
-# #     os.path.join(BASE_DIR, '/static'),
-# # ]
-
-# LOGIN_REDIRECT_URL = 'dashboard'
-# LOGIN_URL = 'login'
-# LOGOUT_URL = 'logout'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# BOOTSTRAP4 = {
-#     'include_jquery': True,
-# }
-
-# ######################################################################
-# ######################################################################
-
 import os
 
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# directory to file manage.py
-
+# directory to file mamnage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))      #sciezka absolutna do pliku manage.py
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 env = environ.Env(
     #defaul types and values
@@ -193,6 +37,7 @@ INSTALLED_APPS = [
     'compressor',
     'taggit',
     'bootstrap4',
+    'crispy_forms',
     'tempus_dominus',
     'reservation.apps.ReservationConfig',
     'homepage.apps.HomepageConfig',
@@ -288,13 +133,12 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 SASS_PROCESOR_ROOT = STATIC_ROOT
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, '/static'),
+# ]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
@@ -305,3 +149,165 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 BOOTSTRAP4 = {
     'include_jquery': True,
 }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# ######################################################################
+# ######################################################################
+
+# import os
+
+# import environ
+
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# # directory to file manage.py
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))      #sciezka absolutna do pliku manage.py
+# # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# env = environ.Env(
+#     #defaul types and values
+#     DEBUG=(bool, False),
+#     ALLOWED_HOSTS=(list, []),
+# )
+# # .env is in parent directory to 'settings.py'
+# env_file= os.path.join(BASE_DIR, '.env')     #sciezka absolutna do pliku env
+# # read from .env file if it exists
+# environ.Env.read_env(env_file)     #laduje zmienne z pliku env do naszego srodowiska
+
+# DEBUG = env('DEBUG')
+# SECRET_KEY = env('SECRET_KEY')
+# ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+
+# # Application definition
+
+# INSTALLED_APPS = [
+#     'account.apps.AccountConfig',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'jquery',
+#     'debug_toolbar',            # bardzo wazne, zeby to bylo za staticfiles!
+#     'django_extensions',
+#     'sass_processor',
+#     'compressor',
+#     'taggit',
+#    # 'bootstrap4',
+#     'crispy_forms',
+#     'tempus_dominus',
+#     'reservation.apps.ReservationConfig',
+#     'homepage.apps.HomepageConfig',
+#     'menu.apps.MenuConfig',
+#     'blog.apps.BlogConfig',
+
+# ]
+
+# MIDDLEWARE = [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',      # <---- to tez dopisujemy na samej gorze to doinsatalowaniu paczek
+#     'django.middleware.security.SecurityMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'restaurant.urls'
+
+# INTERNAL_IPS = [
+#     '127.0.0.1', 
+# ]
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'restaurant.wsgi.application'
+
+# # Database
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+#     # zamieniamy dla heroku tak jak ponizej:
+#     'default': env.db(),
+# }
+
+# # Password validation
+# # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+# # Internationalization
+# # https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_L10N = True
+# USE_TZ = True
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+# STATICFILES_FINDERS = [         
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#     'sass_processor.finders.CssFinder',
+#     'compressor.finders.CompressorFinder',
+    
+# ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+# SASS_PROCESOR_ROOT = STATIC_ROOT
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# LOGIN_REDIRECT_URL = 'dashboard'
+# LOGIN_URL = 'login'
+# LOGOUT_URL = 'logout'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# #BOOTSTRAP4 = {
+# #   'include_jquery': True,
+# #}
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
