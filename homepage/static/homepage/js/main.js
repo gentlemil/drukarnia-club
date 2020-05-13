81331c842e13303b7cb1cb22d0aabad50ebdc8d9
Milosz Bukala <mil.bukala@gmail.com>
2020-05-13 21:13:56 +0200

    This is the end.. for today.. see you tomorrow my dear Git.

--- homepage/static/homepage/js/main.js
+++ homepage/static/homepage/js/main.js
@@ -4,30 +4,46 @@ document.addEventListener("DOMContentLoaded", function () {
     // -------------------------
 
     // Toggle menu on click
-    document.querySelector('#menu-toggler').addEventListener('click', (e) => {
-        toggleBodyClass('menu-active');
+    // document.querySelector('#menu-toggler').addEventListener('click', (e) => {
+    //     toggleBodyClass('menu-active');
+    // });
+
+    // function toggleBodyClass(className) {
+    //     document.body.classList.toggle(className);
+    // }
+
+    // $(window).on('scroll', function () {
+    //     if (window.pageYOffset > 0) {
+    //         $('.menu').addClass('.active-scroll');
+    //     } else {
+    //         $('.menu').removeClass('.active-scroll');
+    //     }
+    // })
+
+    // window.addEventListener('scroll', function () {
+    //     let navigation = document.querySelector('menu-bar')
+    //     if (this.window.pageYOffset > 0) {
+    //         navigation.classList.add('active-scroll')
+    //     } else {
+    //         navigation.classList.remove('active-scroll')
+    //     }
+    // })
+
+    $(document).ready(function () {
+
+        $('.first-button').on('click', function () {
+
+            $('.animated-icon1').toggleClass('open');
+        });
+        $('.second-button').on('click', function () {
+
+            $('.animated-icon2').toggleClass('open');
+        });
+        $('.third-button').on('click', function () {
+
+            $('.animated-icon3').toggleClass('open');
+        });
     });
-
-    function toggleBodyClass(className) {
-        document.body.classList.toggle(className);
-    }
-
-    $(window).on('scroll', function () {
-        if (window.pageYOffset > 0) {
-            $('.menu').addClass('.active-scroll');
-        } else {
-            $('.menu').removeClass('.active-scroll');
-        }
-    })
-
-    window.addEventListener('scroll', function () {
-        let navigation = document.querySelector('menu-bar')
-        if (this.window.pageYOffset > 0) {
-            navigation.classList.add('active-scroll')
-        } else {
-            navigation.classList.remove('active-scroll')
-        }
-    })
 
 
 });
